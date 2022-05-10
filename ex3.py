48be634278e1b1ee561b0477061b1140e9619e53
kenantautges <103221428+kenantautges@users.noreply.github.com>
2022-05-09 19:18:42 -0500

    committing first three exercises from lpthw

---
+++ ex3.py
@@ -0,0 +1,24 @@
+#this line below displays the statement
+print("I will now count my chickens:")
+#these two lines below first displays a word/statement, and then displays the math result
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+#this line below displays the statement
+print("Now I will count the eggs:")
+#this line below performs the math calculations and then displays the result
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#this line below displays the statement
+print("Is it true that 3 +2 < 5 - 7?")
+#this line below performs the math calculations and then displays the result
+print(3 + 2 < 5 - 7)
+#these two lines below first displays a word/statement, and then displays the math result
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+#this line below displays the statement
+print("Oh, that's why it's False.")
+#this line below displays the statement
+print("How about some more.")
+#these lines below displays the statements, performs the math calculations and then displays the results
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
